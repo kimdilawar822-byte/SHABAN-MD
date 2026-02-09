72b2b6146f8d22e2fe124740290f2e20f672e5ba
kimdilawar822-byte <kimdilawar822@gmail.com>
2026-02-09 20:11:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const staticConfig = {
     // ==================== BAILEYS & SESSION ====================
     BAILEYS: "@whiskeysockets/baileys",
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "SHABAN~0hH6",
     CDN: process.env.CDN || "https://bandaheali-cdn.koyeb.app",
 };
 
